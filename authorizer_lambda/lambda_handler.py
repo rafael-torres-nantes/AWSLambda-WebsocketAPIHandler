ea67f339c13ae1d4965f697d5e138815cd5e7e97
rafael-torres-nantes <rafatorresnantes@gmail.com>
2025-05-01 20:19:34 -0400

    feat: add authorization Lambda function and environment configuration

---
+++ authorizer_lambda/lambda_handler.py
@@ -0,0 +1,43 @@
+import json
+import os
+
+# Nome da tabela DynamoDB onde os logs serão registrados
+ARN_LAMBDA_SEND_FEEDBACK = os.getenv('ARN_LAMBDA_SEND_FEEDBACK')
+ARN_LAMBDA_GET_BRIEFING = os.getenv('ARN_LAMBDA_GET_BRIEFING')
+ARN_API_LAMBDA_CONNECT = os.getenv("ARN_API_CONNECT")
+ARN_API_LAMBDA_DISCONNECT = os.getenv("ARN_API_DISCONNECT")
+HASH_KEY = os.getenv('HASH_KEY')
+
+# --------------------------------------------------------------------
+# Função Lambda que processa a requisição de autorização
+# ----------------------------------------------------------------
+def lambda_handler(event, context):
+    # 1 - Log the event
+    print('*********** The event is: ***************')
+    print(event)
+    
+    # 2 - See if the person's token is valid
+    auth = 'Deny'
+    if event['headers']['authorization'] == HASH_KEY:
+        auth = 'Allow'
+    
+    # 3 - Construct and return the response
+    authResponse = {
+        "principalId": "abc123",
+        "policyDocument": {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Action": "execute-api:Invoke",
+                    "Resource": [
+                        ARN_LAMBDA_SEND_FEEDBACK,
+                        ARN_LAMBDA_GET_BRIEFING,
+                        ARN_API_LAMBDA_CONNECT,
+                        ARN_API_LAMBDA_DISCONNECT
+                    ],
+                    "Effect": auth
+                }
+            ]
+        }
+    }
+    return authResponse
